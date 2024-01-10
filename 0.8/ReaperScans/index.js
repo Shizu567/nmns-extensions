6e73a847c924f46f8578b426232fcd1aabbd398f
Shizu567 <124355315+Shizu567@users.noreply.github.com>
2024-01-10 09:30:13 +0530

    Update index.js

--- 0.8/ReaperScans/index.js
+++ 0.8/ReaperScans/index.js
@@ -470,7 +470,7 @@ exports.ReaperScansInfo = {
     name: 'ReaperScans',
     description: 'Reaperscans source for 0.8',
     author: 'NmN',
-    authorWebsite: 'http://github.com/pandeynmm',
+    authorWebsite: 'http://github.com/Shizu567',
     icon: 'icon.png',
     contentRating: types_1.ContentRating.EVERYONE,
     websiteBaseURL: REAPERSCANS_DOMAIN,
